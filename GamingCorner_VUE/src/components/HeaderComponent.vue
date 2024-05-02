79d799eb8ba01ff81f0adb0c08e5ff5c326c6948
IvanBadia2003 <ibadiamiguel@gmail.com>
2024-05-02 18:34:28 +0200

    creating Tarjet Component

---
+++ GamingCorner_VUE/src/components/HeaderComponent.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+    <div>
+
+    </div>
+
+</template>
+
+<style scoped>
+div {
+    width: 100%;
+    height: 130px;
+    background-color: red;
+    position: fixed;
+    animation: contract both;
+    animation-timeline: scroll(root);
+    animation-range: 0 200px;
+}
+
+@keyframes contract {
+    to {
+        background-color: rgba(255, 255, 255, .4);
+        height: 75px;
+        backdrop-filter: blur(9px);
+
+    }
+}
+
+</style>
---
+++ GamingCorner_VUE/src/components/Tarjet.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<style scoped>
+    div {
+        height: 300px;
+        width: 100%;
+        background-color: blue;
+        border-radius: 8px
+    }
+</style>
