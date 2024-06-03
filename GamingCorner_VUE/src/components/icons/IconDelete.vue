5b71913aa31320afd139822e3a130e04cba7d908
IvanBadia2003 <ibadiamiguel@gmail.com>
2024-06-03 17:40:18 +0200

    finishing cartView

---
+++ GamingCorner_VUE/src/components/icons/IconDelete.vue
@@ -0,0 +1,4 @@
+<template>
+    <img src="../../assets/iconDelete.png" alt="Logo">
+  </template>
+  
--- GamingCorner_VUE/src/views/CartView.vue
+++ GamingCorner_VUE/src/views/CartView.vue
@@ -1,37 +1,40 @@
-<template>
-    <div>
-        <h2>Carrito de Compras</h2>
-        <ul>
-            <li v-for="item in cartStore.cartItems" :key="item.videogameId">
-                <img :src="item.imageURL" alt="Game Image" width="50" />
-                <span>{{ item.name }} - {{ item.quantity }} x {{ item.price }} €</span>
-                <button @click="removeFromCart(item.videogameId)">Eliminar</button>
-            </li>
-        </ul>
-        <p><strong>Total:</strong> {{ totalPrice }} €</p>
-        <p><strong>Total Artículos:</strong> {{ totalItems }}</p>
-        <button @click="clearCart">Vaciar Carrito</button>
-    </div>
-</template>
-
 <script setup lang="ts">
 import { useCartStore } from '@/stores/CartStore';
 import { computed } from 'vue';
+import Cart from '@/components/Cart/Cart.vue';
+import Summary from '@/components/Cart/Summary.vue';
 
 const cartStore = useCartStore();
 const cartItems = computed(() => cartStore.cartItems);
 const totalPrice = computed(() => cartStore.totalPrice);
 const totalItems = computed(() => cartStore.totalItems);
 
-const removeFromCart = (videogameId: number) => {
-    cartStore.removeFromCart(videogameId);
-};
 
-const clearCart = () => {
-    cartStore.clearCart();
-};
+
 </script>
 
-<style scoped>
-/* Estilos para el componente */
+<template>
+    <div class="container">
+        <div>
+            <Cart/>
+        </div>
+        <div>
+            <Summary/>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+.container{
+    display: flex;
+    flex-direction: column;
+
+    div{
+        background-color: gray;
+        width: 100%;
+        height: auto;
+    }
+}
+
 </style>
