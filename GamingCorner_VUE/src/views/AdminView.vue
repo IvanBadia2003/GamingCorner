a7261cd3ece566dbea876f7603fd88900d5433de
IvanBadia2003 <ibadiamiguel@gmail.com>
2024-09-01 22:56:39 +0200

    add a graphic

--- GamingCorner_VUE/src/views/AdminView.vue
+++ GamingCorner_VUE/src/views/AdminView.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue';
 import GameAdmin from '../components/Admin/GameAdmin.vue';
 import CreateGame from '../components/Admin/CreateGame.vue';
+import Graphics from '@/components/Admin/Graphics.vue';
 /* import { useUserStore } from '@/stores/UserStore';
 
 const loginStore = useUserStore();
@@ -15,7 +16,7 @@ const opcionSeleccionada = ref('opcion1'); // Valor por defecto
 </script>
 
 <template>
-    <div class="pago">
+    <div class="admin">
         <div>
             <input type="radio" id="opcion1" name="opciones" value="opcion1" v-model="opcionSeleccionada">
             <label for="opcion1">CREAR</label>
@@ -24,32 +25,33 @@ const opcionSeleccionada = ref('opcion1'); // Valor por defecto
         <div>
             <input type="radio" id="opcion2" name="opciones" value="opcion2" v-model="opcionSeleccionada">
             <label for="opcion2">EDITAR / BORRAR</label>
-
-
         </div>
+        <div>
+            <input type="radio" id="opcion3" name="opciones" value="opcion3" v-model="opcionSeleccionada">
+            <label for="opcion3">GRÁFICAS</label>
+        </div>
+
         <div id="menuOpcion1" class="menu" v-show="opcionSeleccionada === 'opcion1'">
             <CreateGame />
-
-
         </div>
 
         <div id="menuOpcion2" class="menu" v-show="opcionSeleccionada === 'opcion2'">
-
             <GameAdmin />
-
-
         </div>
 
-<!--         <div class="divLogout">
-            <RouterLink :to="'/'" @click="logout" class="logout">LOGOUT</RouterLink>
-        </div> -->
+        <div id="menuOpcion3" class="menu" v-show="opcionSeleccionada === 'opcion3'">
+            <Graphics />
+        </div>
 
     </div>
 
 </template>
 
 <style scoped lang="scss">
-.pago {
+.admin {
+    margin-top: 100px;
+    min-height: 70vh;
+
     .divLogout {
         padding: 20px;
 
